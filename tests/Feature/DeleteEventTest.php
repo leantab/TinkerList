b031ddf5fe4ab46577b064c04df0d356f7869e48
Leandro Tabaj <tabajleandro@gmail.com>
2024-03-24 17:22:45 -0300

    tests for Events CRUD all green

--- tests/Feature/DeleteEventTest.php
+++ tests/Feature/DeleteEventTest.php
@@ -10,11 +10,11 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Queue;
 use Tests\TestCase;
 
-class UpdateEventTest extends TestCase
+class DeleteEventTest extends TestCase
 {
     use WithFaker, RefreshDatabase;
 
-    public function test_event_updated(): void
+    public function test_create_and_delete_event(): void
     {
         Queue::fake()->except([
             GetWeatherInfoForEventJob::class,
@@ -22,10 +22,9 @@ class UpdateEventTest extends TestCase
 
         $user = User::factory()->create();
 
-        $location = 'Madrid';
+        $location = 'Paris';
         $email1 = $this->faker->safeEmail();
         $email2 = $this->faker->safeEmail();
-        $email3 = $this->faker->safeEmail();
         
         $response = $this->actingAs($user, 'api')
             ->withSession(['banned' => false])
@@ -37,7 +36,6 @@ class UpdateEventTest extends TestCase
                 'invitees' => [$email1, $email2],
             ]);
 
-        // $response->dump();
         $response->assertStatus(201);
         
         $eventId = $response->json('id');
@@ -45,36 +43,13 @@ class UpdateEventTest extends TestCase
         $response = $this->actingAs($user, 'api')
             ->withSession(['banned' => false])
             ->withHeaders(['Accept', 'application/json'])
-            ->put('/api/events/'.$eventId, [
-                'name' => 'Updated Test Event',
-                'location_name' => $location,
-                'date_time' => '2024-04-04 14:00:00',
-                'invitees' => [$email1, $email3],
-            ]);
+            ->delete('/api/events/'.$eventId);
         
         // $response->dump();
         $response->assertStatus(200);
 
-        $this->assertDatabaseHas('calendar_events', [
-            'name' => 'Updated Test Event',
+        $this->assertDatabaseMissing('calendar_events', [
+            'name' => 'Test Event',
         ]);
-
-        $this->assertDatabaseHas('calendar_event_attendees', [
-            'calendar_event_id' => $eventId,
-            'user_id' => User::where('email', $email1)->first()->id,
-        ]);
-
-        $this->assertDatabaseHas('calendar_event_attendees', [
-            'calendar_event_id' => $eventId,
-            'user_id' => User::where('email', $email3)->first()->id,
-        ]);
-        
-        $this->assertDatabaseMissing('calendar_event_attendees', [
-            'calendar_event_id' => $eventId,
-            'user_id' => User::where('email', $email2)->first()->id,
-        ]);
-
-        Queue::assertPushed(SendEmailInvitationJob::class);
-        // Queue::assertPushed(GetWeatherInfoForEventJob::class);
     }
 }
